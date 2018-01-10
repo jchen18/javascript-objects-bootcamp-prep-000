c4deb9cfcb3a1ae0d33072855328d03912458182
Jessie Chen <jessiechen0513@gmail.com>
2018-01-10 16:58:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new 
 Object({zayn:'you and i'},{ph1:'hiphop'})
 
-function updatePlaylist (object, key, value){
+function updatePlaylist (playlist, artistName, songTitle){
   Object.assign({},object, {[key]: value})
   return object
 }
